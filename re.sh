9e56691c70297c8f33c0d6debfe896a6dc085bd5
Benyomin Walters <chassidus@gmail.com>
2018-04-16 14:19:11 -0500

    Proof of concept version.

--- re.sh
+++ re.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
-CONFIG="$1"
-COMMAND="$2"
-OK=false
 
-if [ $# -ne 2]
-then
-  echo "Usage: $0 {virtual-host} {restart|reload}"
-  echo "Reloads a target virtual host"
-  exit 1
-fi
-
-# reload is allowed
-if [ "$COMMAND" == "reload"]
-then
-  OK=true
-fi
-
-# restart is allowed
-if [ "$COMMAND" == "restart"]
-then
-  OK=true
-fi
-
-# reject any service comman that was not white listed
-if [ "$OK" == false ]
-then
-  echo "Usage: $0 $CONFIG {restart|reload}"
-  exit 1
-fi
+# Move the current execution state to the proper directory
+cd /etc/apache2/sites-available
 
 # Disable all virtual host configurations
-sudo a2dissite "$CONFIG"
-sudo service apache2 restart "$COMMAND"
+sudo a2dissite *
+sudo service apache2 restart
 
 # Enable all virtual host configuration
-sudo a2ensite "$CONFIG"
-sudo service apache2 restart "$COMMAND"
+sudo a2ensite *
+sudo service apache2 restart
